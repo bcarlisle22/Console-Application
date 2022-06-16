404bf92a53b2dc7f8e3dbaea76f1f32ce2a06cc8
Brianna Carlisle <103350106+bcarlisle22@users.noreply.github.com>
2022-06-16 14:25:52 -0500

    Update CIS110_Course_Project.py

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -3,7 +3,7 @@ def typingPrint (text):
     for character in text: 
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.09)
+        time.sleep(0.06)
 
 
 import time, os, sys
@@ -11,12 +11,19 @@ def typingInput (text):
     for character in text: 
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.09)
+        time.sleep(0.06)
 
 typingPrint("Hey there! I have a fun interactive story to share with you! ")
 typingPrint("First, I need to ask a few questions. ")
+
+print("\n")
+
 typingPrint("This story is going to be based on you and a few of your favorite things. ")
-typingPrint("Don't forget, to submit your reply, simply press 'enter.' Let's get started! ")
+typingPrint("Don't forget, to submit your reply, simply press 'enter'. ")
+
+typingPrint("\n")
+
+typingPrint("Let's get started!")
 
 typingPrint("\n")
 
@@ -68,26 +75,29 @@ while(len(favoriteNumber) ==0):
 
 typingPrint("\n")
 
-typingPrint("This is gonna be fun! Let's get started! ")
+typingPrint("Let's start the story! ")
+
+print("\n")
+print("------------------------------------------------------------")
+print("\n")
 
 typingPrint("This is a story about a yorkie named " + dogName +". ")
 
 typingPrint("\n")
 
-typingPrint(dogName + " was left alone for a night while her owner, " +  userName +" went out.")
-typingPrint("Today was " + dogName + "'s opportunity to have a night out herself. So, she decided to roam the streets of " + majorCity)
-typingPrint("to find this new pet spa she heard about. ")
+typingPrint(dogName + " was left alone for a night while her owner, " +  userName + ", went out.")
+typingPrint(" Today was " + dogName + "'s opportunity to have a night out herself. So, she decided to roam the streets of " + majorCity)
+typingPrint(" to find this new pet spa she heard about. ")
 typingPrint(dogName + " waited a while after her owner had left, then she ran out of the doggy door and out into the busy streets.")
 
-typingPrint("\n")
+print("\n")
 
 typingPrint("While walking down the sidewalk, " + dogName + " arrived at " + resturantName + " . And she couldn't help noticing all the ")
 typingPrint(favoriteDessert + "s being made. 'Yummmmm', She thought as she ran to the window and starred. ")
 
-sneakIntoResturant = input("Should " + str(dogName) + " sneak into the resturant ? Type yes or no: ")
+print("\n")
 
-typingPrint("\n")
-
+sneakIntoResturant = input("Should " + str(dogName) + " sneak into the resturant ? Type 'yes' or 'no': ")
 if sneakIntoResturant == "yes":
    typingPrint(dogName + " ran through the resturant and straight to the ktchen, she had to have a " + favoriteDessert + "!")
    typingPrint("One of the servers noticed " + dogName + " and put some of the delicous dessert on a plate for the dog to enjoy.")
@@ -95,30 +105,61 @@ if sneakIntoResturant == "yes":
    typingPrint("Unfortunately, some of the customers caught sight of the dog enjoying her dessert in the kitchen and it upset them, causing some of them to leave")
    typingPrint("One of the customers even called Animal Rescue to come pick the dog up and take her home. But they were too late. When they got to the returant, the dog was long gone. ")
 else: 
-   typingPrint("The yorkie decided not to go into the resturant because she did not want to get her fur  all dirty.")
+   typingPrint("The yorkie decided not to go into the resturant because she did not want to get her fur all dirty.")
 
-typingPrint("\n")
+print("\n")
 
 typingPrint("After the resturant,"+ dogName + " finally makes it to the pet spa.")
 typingPrint("It was just as she pictures it! A cute little place with dogs like her relaxing and getting pampered!")
 typingPrint(dogName +" was so excited to get here, she forgot to decide what service she even wanted done. A massage? A pawicure?")
 
-NailService= input("Should she get a pawicure or a massage? Type P or M:")
+print("\n")
 
-if NailService == 'P':
+nailService= input("Should she get a pawicure or a massage? Type 'P' or 'M': ")
+
+if nailService.upper() == 'P':
     
-   typingPrint("\n)
+   print("\n")
                
-   typingPrint("Eventually, She decides on a pawicure and gets a glossy coat of pink nail polish on her paws. The yorkie was having so much fun, she forgot she had to be home by 10:00pm!")
-   typingPrint("After spending a day in " + majorCity +", " + dogName + " decided she could'nt run home before " + userName + " got back, because ")
-   typingPrint("she would mess up her nails.")
-   typingPrint("Luckily, when " + userName + " got home and found the yorkie missing, they knew exactly where to find her. ")
-   typingPrint("The silly dog did'nt realize her owner was going to suprise her with a trip to the spa the next day.")
+   typingPrint("Eventually, she decides on a pawicure and gets a glossy coat of pink nail polish on her paws. ")
+   typingPrint("\nThe yorkie was having so much fun, she forgot she had to be home by 10:00! ")
 else: 
-    typingPrint("The yorkie decides to get a massage. All of this running through the city has made her tense")
-    typingPrint("The massage was so relaxing, she fell asleep "+ dogName + ". She had to run home before her owner got back.")
-    typingPrint("Lucky for her, she got home just in time")
+    typingPrint("The yorkie decides to get a massage. All of this running through the city has made her tense.")
+    typingPrint(" The massage was so relaxing, " + dogName + " fell asleep. She woke up in a jolt and ran home as fast as could. ")
+    typingPrint("She had to hurry run home before her owner got back.")
 
-    print("\n")
-    print("\n")
-print("------------ The End--------------")
+Method= input("Should the yorkie run home or stay at the salon? Type 'R' or 'S': ")
+    
+if nailService.upper() == 'P' and  Method == 'S':
+   
+   print("\n")
+   
+   typingPrint("It was 9:58 and " + dogName + " didn't think she would make it home before " + userName + " got back. ")
+   typingPrint("Besides, she would mess up her nails. So she waited and waited hoping her owner would come get her. " )
+   print("\n")
+   typingPrint("Luckily, when " + userName + " got home and found the yorkie missing, " + userName+ " knew exactly where to find her. ")
+   typingPrint("When " + userName + " got to the salon, they picked " + dogName + " up and said ")
+   typingPrint("'You know it's not safe to roam the streets by yourself.")
+   typingPrint("..and you didnt need to.' ")
+   typingPrint("\n")
+   typingPrint(dogName + " was confused")
+   print("\n")
+   typingPrint(userName+ " explained it to her on the way home. ")
+   typingPrint("The silly dog didn't need to sneak out to the spa because her owner was going to suprise her with a trip to the spa the next day.")
+else: 
+   print("\n")
+   typingPrint(dogName+ " ran home as fast as she could before her owner got back. ")
+   typingPrint("Lucky for her, she got home just in time! ")
+   print("\n")
+   typingPrint(dogName + " was already snugged up in bed when " + userName + " opened the front door. ")
+   typingPrint("'That was close' " + dogName + " thought. As their owner came closer to greet them. ")
+   typingPrint( userName + " looked at the dog and said, 'Hmmmm, something smells like......nail polish.' ")
+   typingPrint("BUSTED!")
+   
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+
+print("------------------------ The End-----------------------------")
