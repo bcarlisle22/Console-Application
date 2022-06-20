d209fb0e720e521ffaed4eb1838551dc574f96f7
Brianna Carlisle <103350106+bcarlisle22@users.noreply.github.com>
2022-06-19 22:47:14 -0500

    Update CIS110_Course_Project.py

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -5,14 +5,6 @@ def typingPrint (text):
         sys.stdout.flush()
         time.sleep(0.06)
 
-
-import time, os, sys
-def typingInput (text):
-    for character in text: 
-        sys.stdout.write(character)
-        sys.stdout.flush()
-        time.sleep(0.06)
-
         
 keepGoing="y"
 while keepGoing.lower()== "y":
